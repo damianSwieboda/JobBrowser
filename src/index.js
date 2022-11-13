8759be57a9cdf797046c749cfa820bcc13096baf
Damian Świeboda <damianswieboda07@gmail.com>
2022-11-13 01:05:09 +0100

    empty routers, public directory, plain login form -  css and html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
 
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+ 
+app.listen(port, ()=>{
+    console.log(`App listen on port ${port}`)
+})
